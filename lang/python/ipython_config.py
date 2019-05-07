449810f49c582eac52eac44c736a2a9563d29241
SuJiKiNen <sujikinen@gmail.com>
2019-05-08 00:20:43 +0800

    Tell where IPython was running

--- lang/python/ipython_config.py
+++ lang/python/ipython_config.py
@@ -580,3 +580,4 @@ c.StoreMagics.autorestore = True
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 c.InteractiveShellApp.exec_lines.append('print("Warning: disable autoreload in ipython_config.py to improve performance.")')
+c.InteractiveShellApp.exec_lines.append('import sys; print("Running IPython from: " + " ".join(sys.argv))')
