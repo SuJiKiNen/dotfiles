99cb7131e59182d73708ca966bc07b7121cc842f
SuJiKiNen <sujikinen@gmail.com>
2019-06-30 23:24:55 +0800

    Use -V flag,this more compatible

--- shell/bash/prompt/python_version.bash
+++ shell/bash/prompt/python_version.bash
@@ -2,7 +2,7 @@
 function _python_version_prompt(){
     command -v python > /dev/null 2>&1 || return
     local _py_version
-    _py_version=$(python --version 2>&1 | cut -d ' ' -f2)
+    _py_version=$(python -V 2>&1 | cut -d ' ' -f2)
     local prefix="$1"
     local suffix="$2"
     if [[ -n "$_py_version" ]]; then
