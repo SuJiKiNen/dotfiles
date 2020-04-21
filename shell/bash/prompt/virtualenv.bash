65772994f02c2e865981d4a9c3d8637532ba4454
SuJiKiNen <sujikinen@gmail.com>
2020-04-21 08:54:29 +0800

    Simplify virtualenv prompt
    no need to check virtualenv command existence

--- shell/bash/prompt/virtualenv.bash
+++ shell/bash/prompt/virtualenv.bash
@@ -1,8 +1,7 @@
-function _virtualenv_prompt(){
-    command -v virtualenv > /dev/null 2>&1 || return
-    local prefix="$1"
-    local suffix="$2"
-    if [[ -n "$VIRTUAL_ENV" ]]; then
+function _virtualenv_prompt {
+    if [ -n "$VIRTUAL_ENV" ]; then
+        local prefix="$1"
+        local suffix="$2"
         # Strip out the path and just leave the env name
         printf "%s" "${prefix}${VIRTUAL_ENV##*/}${suffix}"
     fi
